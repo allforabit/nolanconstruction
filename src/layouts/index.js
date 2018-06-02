29aa50b1c3fc4691e94c641328a2e975b5ec90e1
Kevin Nolan <kevnolan@gmail.com>
2018-06-02 15:56:18 +0100

    initial testing

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,12 +5,26 @@ import Helmet from 'react-helmet'
 import Navbar from '../components/Navbar'
 import './all.sass'
 
+import {Provider} from 'rebass';
+
+const theme = {
+  fonts: {
+    sans: '"Avenir Next", Helvetica, sans-serif',
+  },
+  fontSizes: [
+    12, 16, 24, 36, 48, 72
+  ],
+  colors: {
+    blue: "black"
+  }
+}
+
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+  <Provider theme={theme}>
+    <Helmet title="Herb & Bloom" />
     <Navbar />
     <div>{children()}</div>
-  </div>
+  </Provider>
 )
 
 TemplateWrapper.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+// Get rebass
+import { Box, Heading, Button } from 'rebass'
 
 export default class IndexPage extends React.Component {
   render() {
@@ -8,37 +10,10 @@ export default class IndexPage extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-          </div>
-          {posts
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
-        </div>
-      </section>
+      <Box>
+        <Heading>Hello</Heading>
+        <Button>Rebass</Button>
+      </Box>
     )
   }
 }
