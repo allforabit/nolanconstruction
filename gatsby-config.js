cb7760ed5abd377f7d8cbfe913a87590f541c474
Kevin Nolan <kevnolan@gmail.com>
2018-06-13 14:27:53 +0100

    Added landing.md

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter"
+    title: "HERB AND BLOOM LONDON"
   },
   plugins: [
     "gatsby-plugin-react-next",
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -75,144 +75,15 @@ const theme = {
   }
 };
 
-const Title = styled(Heading)([]);
-// TODO sort out header font
-// TODO sort out general font
-
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.myRef = React.createRef();
-  }
-
-  componentDidMount() {}
-
-  render() {
-    const { style, ...props } = this.props;
-    const logoSizes = [12, 14, 16, 18].map(x => x * 16);
-    return (
-      <Box
-          bg="blue"
-        >
-        <Banner
-          style={{
-            WebkitFontSmoothing: "antialiased"
-          }}
-          color="white"
-          ratio={2 / 3}
-          // backgroundSize="100px 100px"
-        >
-          <Box width={logoSizes} height={100}>
-            <Logo size={logoSizes} color="white" />
-            <Box mt={5} color="purple">
-              <Text textAlign="center" fontSize={[2, null, 3]}>
-                Grown in London.
-              </Text>
-              <Text textAlign="center" fontSize={[2, null, 3]}>
-                For London.
-              </Text>
-            </Box>
-          </Box>
-        </Banner>
-        <Banner
-          bg="blue"
-          bgImage={pattern}
-          backgroundSize="auto"
-          minHeight={[192, null, 256]}
-        />
-      </Box>
-    );
-  }
-}
-
-const StyledObfuscate = sys({
-  is: Obfuscate,
-  color: "purple",
-  hover: {
-    color: "black"
-  }
-  // fontWeight: "bold"
-});
-
-const BrandLink = () => (
-  <Link href="https://herbandbloom.co.uk">Herb & Bloom</Link>
-);
-
-const StyledBrandLink = sys({
-  is: Link,
-  fontFamily: "heading",
-  fontSize: 2,
-  textDecoration: "overline"
-});
-
-const BelowHeader = props => (
-  <Box
-    style={{
-      WebkitFontSmoothing: "antialiased"
-    }}
-    color="blue"
-    pb={4}
-    pt={4}
-  >
-    <Container flexWrap="wrap" width={1} maxWidth={640} fontSize={[1, null, 2]}>
-      <Text textAlign="center" mb={[3, null, 4]}>
-        Herb & Bloom is an innovative vertical farming business opening summer
-        2018 in Battersea.
-      </Text>
-      <Text textAlign="center" mb={[3, null, 4]}>
-        Developed by two hosptiality professionals and supported by the
-        industrys leading indoor farming engineers, we will be delivering the
-        freshest, premium culinary herbs & edible flowers to Londons restaurants
-        and local communities.
-      </Text>
-      <Text textAlign="center" mb={[3, null, 4]}>
-        We strive for an open door policy to all interested parties. If you'd
-        like to come to our showcase, or have any other questions drop us a line
-        at <StyledObfuscate email="info@herbandbloomlondon.co.uk" />
-      </Text>
-    </Container>
-  </Box>
-);
-
-const HBIcon = () => {
-  return <Image src={hbIconImg} width={96} />;
-};
-
-const Footer = props => (
-  <Container>
-    <Box color="black" mt={3} py={3} fontSize={1}>
-      <Divider w={1} borderColor="white" />
-      <Flex alignItems="center" justifyContent="space-between">
-        <Box>
-          <Root
-            fontFamily="heading"
-            style={{
-              WebkitFontSmoothing: "antialiased"
-            }}
-          >
-            <Text fontSize={[2, null, 3]} color="blue">
-              © 2018 HERB AND BLOOM
-            </Text>
-          </Root>
-        </Box>
-        <Box>
-          <HBIcon />
-        </Box>
-      </Flex>
-    </Box>
-  </Container>
-);
-
 const TemplateWrapper = ({ children }) => (
   <Provider theme={theme}>
     <Root fontFamily="sans" bg="grey">
       <Helmet title="Herb & Bloom" />
-      <Header />
-      <BelowHeader />
-      <Footer />
+      {children()}
     </Root>
   </Provider>
 );
+
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func
