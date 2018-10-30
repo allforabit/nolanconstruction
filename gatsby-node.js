bf17de01d60f2d3220a2fa586d367bf220cfb2cf
Kevin Nolan <kevnolan@gmail.com>
2018-10-30 15:10:08 +0000

    Fix mapbox

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,4 +14,17 @@ exports.onCreateWebpackConfig = ({
       plugins: [new DirectoryNamedWebpackPlugin()],
     },
   });
+
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /mapbox-gl/,
+            use: ['null-loader'],
+          },
+        ],
+      },
+    });
+  }
 };
